c0c2f1860f80d6c73ffb405233fe0ead3d29d163
Girish Ramesh <rgirish28@gmail.com>
2013-03-06 14:05:08 +0530

    Corrected error in picture.rb

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,4 @@
 class Picture < ActiveRecord::Base
   belongs_to :imageable
-  has_many :pictures
   attr_accessible :name
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :author
+  has_many :pictures
   has_and_belongs_to_many :tags
   attr_accessible :content, :likes, :posted, :shares, :title
 end
