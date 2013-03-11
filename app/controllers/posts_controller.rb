f4006f56854c49de0ec9447865b2f34e120babc9
Girish Ramesh <rgirish28@gmail.com>
2013-03-11 18:02:46 +0530

    Error

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  @post = Post.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 BlogApp::Application.routes.draw do
+  
+
+  root :to => 'welcome#index'
   resources :authors do
-    resources :posts do
-      resources :comments
-    end
+    resources :posts 
   end
   resources :tags
 
@@ -55,7 +56,7 @@ BlogApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
